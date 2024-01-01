b2c50855c789da3d48473ef37bb55815faee9e13
Adisa Ade <abubakradisa@gmail.com>
2024-01-01 09:54:13 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <body class="h-screen scroll-smooth">
+    
+  </body>
+</template>
+<style scoped>
+@media screen and (max-width: 767px) {
+  body{
+    background-image: url('./assets/home/background-home-mobile.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }  
+}
+@media screen and (min-width:768px) and (max-width: 1023px) {
+  body{
+    background-image: url('./assets/home/background-home-tablet.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+}
+@media screen and (min-width: 1024px) {
+  body{
+    background-image: url('./assets/home/background-home-desktop.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+}
+</style>
