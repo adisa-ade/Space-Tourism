17ad83bfb5389c9c02c711499cf26d2c03a5c2d0
Adisa Ade <abubakradisa@gmail.com>
2024-01-01 13:47:31 +0100

    initial commit

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,16 @@
 <script setup>
-    import {RouterLink} from 'vue-router'
+    import {RouterLink} from 'vue-router'    
+    import {ref} from 'vue'
+    const showNavBar = ref(true)
 </script>
 <template>
     <header class="flex justify-between items-center p-6">
         <img src="../assets/shared/logo.svg" alt="">
-        <div class="mobile-toggle">
-        <img src="../assets/shared/icon-hamburger.svg" alt="">
-        <img class="hidden" src="../assets/shared/icon-close.svg" alt="">
+        <div class="mobile-toggle z-10">               
+        <img v-if="showNavBar" @click="showNavBar = false" src="../assets/shared/icon-hamburger.svg" alt="">
+        <img v-if="!showNavBar" :style="{display:'block'}" @click="showNavBar = true" class="hidden" src="../assets/shared/icon-close.svg" alt="">
         </div>
-        <div class="desktop-nav flex gap-6 bg-gradient-to-b from-gray-500 to-gray-200 p-6 opacity-90">
+        <div class="desktop-nav flex gap-6 p-6 m-0">
             <RouterLink to="" class="text-lg font-bold">00 HOME</RouterLink>
             <RouterLink to="" class="text-lg font-bold">01 DESTINATION</RouterLink>
             <RouterLink to="" class="text-lg font-bold">02 CREW</RouterLink>
@@ -16,23 +18,38 @@
             </div>
     </header>
     <nav>
-        <div  class="mobile-nav flex flex-col bg-gradient-to-b from-gray-800 to-gray-300 py-20 px-10 space-y-10 w-3/4 h-screen absolute top-0 right-0">
+        <div v-if="!showNavBar" :style="{display:'flex'}" class="mobile-nav hidden flex-col bg-gradient-to-b from-gray-800 to-gray-300 py-20 px-10 space-y-10 w-3/4 h-screen absolute top-0 right-0 z-0">
         <RouterLink to="" class="text-lg font-bold">00 HOME</RouterLink>
         <RouterLink to="" class="text-lg font-bold">01 DESTINATION</RouterLink>
         <RouterLink to="" class="text-lg font-bold">02 CREW</RouterLink>
         <RouterLink to="" class="text-lg font-bold">03 TECHNOLOGY</RouterLink>
         </div>        
-    </nav>
+    </nav>    
 </template>
 <style scoped>
 @media screen and (max-width: 767px) {
     .desktop-nav{
         display: none;
     }
+    .mobile-toggle img{
+        animation: smooth 0.5s ease-in;
+    }
+    @keyframes smooth {
+        0%{
+            opacity: 0;            
+        }
+        100%{
+            opacity: 1;        
+        }
+        
+    }
 }
 @media screen and (min-width: 768px) {
     .mobile-toggle,.mobile-nav{
         display: none;
+    }
+    .desktop-nav{
+        background-color: #292e3a;        
     }
 }
 </style>
